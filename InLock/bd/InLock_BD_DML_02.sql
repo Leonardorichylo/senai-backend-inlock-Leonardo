ff0970be586725f48d1fc7632f8395db0761eef2
FIC <FIC@info.local>
2020-03-02 11:28:32 -0300

    02/03

---
+++ InLock/bd/InLock_BD_DML_02.sql
@@ -0,0 +1,22 @@
+USE InLock_Games_Manha;
+
+
+INSERT INTO TiposUsuario (TituloTipoUsuario)
+VALUES ('Administrador')
+,('Cliente')
+
+
+INSERT INTO Usuarios (Email,Senha,IdTipoUsuario)
+VALUES ('admin@admin.com','admin',1)
+,('cliente@cliente.com','cliente',2)
+
+
+INSERT INTO Studio (NomeStudio)
+VALUES	('Blizzard')
+,('Rockstar Studios')
+,('Square Enix')
+
+
+INSERT INTO Jogos (NomeJogo,DataLancamento,Descricao,IdStudio,Valor)
+VALUES	('Diablo 3','15-05-2012','é um jogo que contém bastante ação e é viciante, seja um novato ou um fã',1,'R$99,00')
+,('Red Dead Redemption II', '26-10-2018','jogo eletrônico de ação-aventura western',2,'R$120')
---
+++ InLock/bd/InLock_BD_DQL_03.sql
@@ -0,0 +1,8 @@
+USE InLock_Games_Manha;
+
+SELECT * FROM Usuarios;
+
+SELECT * FROM Studio;
+
+SELECT * FROM Jogos;
+--Listar os jogos com seus studios
